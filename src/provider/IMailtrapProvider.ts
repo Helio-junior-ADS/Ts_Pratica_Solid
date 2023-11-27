a43765ec86ea089d481f05eac7a35ae1c3a97c40
Helio-junior-ADS <64769193+Helio-junior-ADS@users.noreply.github.com>
2023-11-27 12:30:21 -0300

    criado o provider mailtrapMailProvider com nodemail

--- src/provider/IMailtrapProvider.ts
+++ src/provider/IMailtrapProvider.ts
@@ -1,3 +1,14 @@
+export interface IAddress {
+  name: string;
+  email: string
+}
+
+export interface IMessage {
+  to:IAddress,
+  from:IAddress,
+  subject: string,
+  body: string
+}
 export interface IMailtraProvider {
-  
+  sendMail(message:IMessage):Promise<void>
 }
--- src/userCases/CreateUser/CreateUserUseCase.ts
+++ src/userCases/CreateUser/CreateUserUseCase.ts
@@ -1,10 +1,12 @@
 import { User } from "../../intities/User";
+import { IMailtraProvider } from "../../provider/IMailtrapProvider";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { CreateUsersRequestDTO } from "./CreateUsersResquestDTO";
 
 export class CreateUserUseCase {
   constructor(
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+    private mailtrapMailProvider: IMailtraProvider
   ){}
 
   async execute(data:CreateUsersRequestDTO): Promise<void>{
@@ -16,6 +18,19 @@ export class CreateUserUseCase {
 
     const user = new User(data);
     await this.usersRepository.save(user);
+
+    await this.mailtrapMailProvider.sendMail({
+      to:{
+        name:'Google',
+        email:'google@google.com'
+      },
+      from:{
+        name:data.name,
+        email:data.email
+      },
+      subject:"Vaga de senior",
+      body: '<p>Parabéns Foi está contratado para vaga de estágio</p>'
+    });
   }
 
 }
