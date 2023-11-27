4ecfa031c5a6f580fe1712a56a3da3f7c72a3168
Helio-junior-ADS <64769193+Helio-junior-ADS@users.noreply.github.com>
2023-11-27 11:22:00 -0300

    feit instalacao de dependencias e setup inicial

--- src/UseCases/CreateUser/CreateUserDTO.ts
+++
@@ -1,5 +0,0 @@
-export interface ICreateUserResquestDTO {
-  name: string;
-  email: string;
-  password: string;
-} 
--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
-import { StatusCodes } from "http-status-codes"
-import { createUserController } from "./UseCases/CreateUser";
-
+import {StatusCodes} from 'http-status-codes'
 
 const router = Router();
 
-router.post('/users', (request, response) => {
-  return createUserController.handle(request, response)
-})
+router.get('/users', (request, response) =>{
+  return response.status(StatusCodes.ACCEPTED).send("Hello word");
+});
 
 export { router };
